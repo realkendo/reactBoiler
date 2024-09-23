73a62f7d66fd765ba3a7a2b2f77fe118a315f17c
realkendo <kennethistifanus1@gmail.com>
2024-09-23 21:37:56 +0100

    css modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,3 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-
-//   return (
-//     <>
-//     <h1>Welcome to React</h1>
-//     <div className="happy">didn't push an update so i had to scramble</div>
-//    </>
-//   )
-// }
-
-// export default App
-
 import React, { useState } from "react";
 import "./App.css"; // Optional for styling
 
