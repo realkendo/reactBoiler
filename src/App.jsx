5b886b26650331b9549c137c947c1671a0435058
realkendo <kennethistifanus1@gmail.com>
2024-11-20 14:51:32 +0100

    redesign

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import React from "react";
 // import Header from "./components/Header";
 // import Footer from "./components/Footer";
 import Card from "./components/card";
+import contacts from "./contacts";
 
 
 function Keeper(){
   return (
     <div style={{display:"flex"}}>
       {/* <Card name="Netflix" img="/images/netflix.png" alt="Brand Logo" type="Streaming Services" email="netflix.com" colors="red" /> */}
-      <Card name="Google" img="/images/google.png" alt="Brand Logo" type="Search Engine" site="google.com" colors="green" />
-      <Card name="Apple" img="/images/apple.png" alt="Brand Logo" type="Personal Computers" site="apple.org" colors="orange" />
+      <Card name={contacts[0].name} img={contacts[0].img}  type={contacts[0].type}  site={contacts[0].site}  colors={contacts[0].colors}  />
+      <Card name={contacts[1].name}  img={contacts[1].img} type={contacts[1].type}  site={contacts[1].site}  colors={contacts[1].colors}  />
       {/* <Card name="Meta" img="/images/meta.png" alt="Brand Logo" type="Social Media" email="facebook.com" colors="blue" /> */}
-      <Card name="Microsoft" img="/images/microsoft.png" alt="Brand Logo" type="Operating System" site="microsoft.com" colors="red" />
+      <Card name={contacts[2].name}  img={contacts[2].img} type={contacts[2].type}  site={contacts[2].site}  colors={contacts[2].colors}  />
     </div>
   )
 }
---
+++ src/contacts.js
@@ -0,0 +1,25 @@
+const contacts = [
+  {
+    name : "Google",
+    img: "/images/google.png",
+    type: "Search Engine",
+    site: "google.com",
+    colors: "green"
+  },
+  {
+    name : "Apple",
+    img: "/images/apple.png",
+    type: "Personal Computers",
+    site: "apple.com",
+    colors: "orange"
+  },
+  {
+    name : "Microsoft",
+    img: "/images/microsoft.png",
+    type: "Operating Systems",
+    site: "microsoft.com",
+    colors: "red"
+  }
+]
+
+export default contacts;
