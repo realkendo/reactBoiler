f6ff11efb537101cd405a221e833ab086815b6e5
realkendo <kennethistifanus1@gmail.com>
2024-11-24 19:44:32 +0100

    mapping contacts

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ import contacts from "./contacts";
 function createCard(contacts){
   return(
     <Card 
-      name={contacts[0].name} 
-      img={contacts[0].img}  
-      type={contacts[0].type}  
-      site={contacts[0].site}  
-      colors={contacts[0].colors}  
+      name={contacts.name} 
+      img={contacts.img}  
+      type={contacts.type}  
+      site={contacts.site}  
+      colors={contacts.colors}  
     />
 
   )
@@ -22,11 +22,12 @@ function createCard(contacts){
 function Keeper(){
   return (
     <div style={{display:"flex"}}>
-      {/* <Card name="Netflix" img="/images/netflix.png" alt="Brand Logo" type="Streaming Services" email="netflix.com" colors="red" /> */}
+      {contacts.map(createCard)}
+      {/* {/* <Card name="Netflix" img="/images/netflix.png" alt="Brand Logo" type="Streaming Services" email="netflix.com" colors="red" />
       <Card name={contacts[0].name} img={contacts[0].img}  type={contacts[0].type}  site={contacts[0].site}  colors={contacts[0].colors}  />
-      <Card name={contacts[1].name}  img={contacts[1].img} type={contacts[1].type}  site={contacts[1].site}  colors={contacts[1].colors}  />
+      <Card name={contacts[1].name}  img={contacts[1].img} type={contacts[1].type}  site={contacts[1].site}  colors={contacts[1].colors}  /> */}
       {/* <Card name="Meta" img="/images/meta.png" alt="Brand Logo" type="Social Media" email="facebook.com" colors="blue" /> */}
-      <Card name={contacts[2].name}  img={contacts[2].img} type={contacts[2].type}  site={contacts[2].site}  colors={contacts[2].colors}  />
+      {/* <Card name={contacts[2].name}  img={contacts[2].img} type={contacts[2].type}  site={contacts[2].site}  colors={contacts[2].colors}  /> */} 
     </div>
   )
 }
