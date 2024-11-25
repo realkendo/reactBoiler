bc1c812f4691067d543ea821f56b6860c5eebff0
realkendo <kennethistifanus1@gmail.com>
2024-11-25 14:26:02 +0100

    monday commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-// import Note from "./components/Note"
+// import Note from "./components/Note";
 // import Header from "./components/Header";
 // import Footer from "./components/Footer";
-import Card from "./components/card";
+import Card from "./components/Card";
 import contacts from "./contacts";
 
 function createCard(contacts){
   return(
     <Card 
-      key={id}
+      key={contacts.id}
       name={contacts.name} 
       img={contacts.img}  
       type={contacts.type}  
@@ -20,15 +20,10 @@ function createCard(contacts){
 }
 
 
-function Keeper(){
+const Keeper = () =>{
   return (
     <div style={{display:"flex"}}>
       {contacts.map(createCard)}
-      {/* {/* <Card name="Netflix" img="/images/netflix.png" alt="Brand Logo" type="Streaming Services" email="netflix.com" colors="red" />
-      <Card name={contacts[0].name} img={contacts[0].img}  type={contacts[0].type}  site={contacts[0].site}  colors={contacts[0].colors}  />
-      <Card name={contacts[1].name}  img={contacts[1].img} type={contacts[1].type}  site={contacts[1].site}  colors={contacts[1].colors}  /> */}
-      {/* <Card name="Meta" img="/images/meta.png" alt="Brand Logo" type="Social Media" email="facebook.com" colors="blue" /> */}
-      {/* <Card name={contacts[2].name}  img={contacts[2].img} type={contacts[2].type}  site={contacts[2].site}  colors={contacts[2].colors}  /> */} 
     </div>
   )
 }
